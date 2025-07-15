68a9176556167d87271ca9c2c68894910dfd9901
Muskann2731 <muskaann1111@gmail.com>
2025-07-15 22:19:41 +0530

    Delete korean skincare directory

--- korean skincare/script.js
+++
@@ -1,8 +0,0 @@
-function showTip() {
-  alert("Tip: Hydrate inside out — drink water and use essence!");
-}
-
-window.addEventListener("scroll", function() {
-  let scroll = window.pageYOffset;
-  document.body.style.backgroundPosition = `center ${scroll * 0.3}px`;
-});
